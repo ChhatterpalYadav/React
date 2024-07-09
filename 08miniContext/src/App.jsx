f74e271cd2ebb76fe7c8a92e78a84e0732d42311
ChhatterpalYadav <cpyadav2308@gmail.com>
2024-07-09 18:16:27 +0530

    commit

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
+function App() {
+  
+
+  return (
+    <>
+    <UserContextProvider>
+    <h1>Chhatterpal</h1>
+    <Login />
+    <Profile />
+    </UserContextProvider>
+      
+    </>
+  )
+}
+
+export default App
